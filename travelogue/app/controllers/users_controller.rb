a29c1a154ab8d3d1409578fb2beb411dba17fbd8
isangieri <isabelleangieri@gmail.com>
2016-01-19 13:30:48 -0800

    Defined method new in users_controller.rb

--- travelogue/app/controllers/users_controller.rb
+++ travelogue/app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
+
   def index
     @users = User.all
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    redirect_to @user
   end
 
   def show
@@ -12,7 +19,10 @@ class UsersController < ApplicationController
   def edit
   end
 
-  def new
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password_digest, :current_city, :profile_photo)
   end
-  
+
 end
