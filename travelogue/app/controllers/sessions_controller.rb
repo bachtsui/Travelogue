4456847dee4835c5a18a8d4fc836f7f6e3379d73
bachtsui <dbtsui2@gmail.com>
2016-01-19 11:58:34 -0800

    Created a new and create method for sessions controller

--- travelogue/app/controllers/sessions_controller.rb
+++ travelogue/app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.confirm(user_params)
+		if @user
+			login(@user)
+			flash[:notice] = "Successfully logged in"
+			redirect_to user_path
+			#Get a blank page atm
+		else
+			flash[:error] = "Incorrect email or password."
+			redirect_to new_session_path
+		end
+
 end
